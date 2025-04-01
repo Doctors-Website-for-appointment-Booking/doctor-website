db94fba044a1e1d77f44e1a27d9803e0afa595cd
Ajay Kumar <145866309+vvermaaj@users.noreply.github.com>
2025-04-01 23:31:24 +0530

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
 // API Configuration
-export const API_BASE_URL = "https://shyamhomeopathy.com";
+export const API_BASE_URL = "https://doctor-website-backend-production.up.railway.app";
 // export const API_BASE_URL = "http://localhost:5353";
 
 export const API_ENDPOINTS = {
