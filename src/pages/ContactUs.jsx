b4c4f293abfb5d0e0223fab70463bf310ea9907f
Ajay Kumar <145866309+vvermaaj@users.noreply.github.com>
2025-04-01 14:51:36 +0530

    Update ContactUs.jsx

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -133,7 +133,7 @@ const ContactUs = () => {
     setIsSubmitting(true);
 
     try {
-      const response = await fetch('https://doctor-website-backend-production.up.railway.app/api/contacts', {
+      const response = await fetch('doctor-website-backend.railway.internal/api/contacts', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
